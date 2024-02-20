1790f9a1c6a8afad42710c579fc63cc2d8aad4b1
Kirill Nesterov <nester.k.a@yandex.ru>
2024-02-20 19:28:26 +0300

    sam

---
+++ 02_HelloKotlin/untitled/src/main/kotlin/samostrabota.kt
@@ -0,0 +1,26 @@
+fun main (){
+
+    val latterCode1 = 75
+    val latterCode2 = 111
+    val latterCode3 = 116
+    val latterCode4 = 108
+    val latterCode5 = 105
+    val latterCode6 = 110
+
+
+    val latter1 = Char(latterCode1)
+    val latter2 = Char(latterCode2)
+    val latter3 = Char(latterCode3)
+    val latter4 = Char(latterCode4)
+    val latter5 = Char(latterCode5)
+    val latter6 = Char(latterCode6)
+
+    print(latter1)
+    print(latter2)
+    print(latter3)
+    print(latter4)
+    print(latter5)
+    print(latter6)
+
+
+}
